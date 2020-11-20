b95f2d4590d7b6634defd6e7f79e96f76b698752
Nil Nguyen <Nilver@air-cua-mac.stusgs.rmit.edu.vn>
2020-11-20 20:07:45 +0700

    Initial Commit

---
+++ CookingRecipe/CookingRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingRecipeApp.swift
+//  CookingRecipe
+//
+//  Created by Nil Nguyen on 11/20/20.
+//
+
+import SwiftUI
+
+@main
+struct CookingRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
